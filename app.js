af28217e8b105f88868549f950dcfb53e68a085a
Junjie Chen <junjie.chen18@gmail.com>
2020-05-27 15:48:53 -0400

    feat(unresolved state): start game

---
+++ app.js
@@ -0,0 +1,19 @@
+function startGame() {
+  let counter = 0;
+
+  const button = document.querySelector('button');
+
+  button.addEventListener('click', () => {
+    ++counter;
+  });
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (counter > 5) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 2000);
+  });
+}
